8c9e0023a835865ca70eca359b2c3febefa77f17
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2023-10-21 03:54:01 +0530

    main

---
+++ ethonline/pages/index.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar/>
+
+      
+      <Footer/>
+  
+  
+  </main>
+  )
+}
