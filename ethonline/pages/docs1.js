655b679b02b322440556568bbd33d1837a97b5a6
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2023-10-22 20:31:45 +0530

    Create docs1.js

---
+++ ethonline/pages/docs1.js
@@ -0,0 +1,16 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+
+export default function Navbar() {
+
+   
+    return (
+
+<>
+<Navbar/>
+
+<Footer/>
+
+</>
+    )}
